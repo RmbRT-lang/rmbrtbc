7c47e073f3a542cc310afe8c9265184812a6a396
RmbRT <steffen.rattay@gmail.com>
2020-01-20 22:04:07 +0100

    Reworked scoped symbols.

--- src/scoper/symbol.c
+++ src/scoper/symbol.c
@@ -5,28 +5,37 @@
 
 #include <string.h>
 
+struct RlcScopedSymbolChild const kRlcScopedSymbolChildConstructor = {
+	kRlcParsedSymbolChildTypeConstructor, NULL
+};
+
+struct RlcScopedSymbolChild const kRlcScopedSymbolChildDestructor = {
+	kRlcParsedSymbolChildTypeDestructor, NULL
+};
+
 void rlc_scoped_symbol_child_create(
 	struct RlcScopedSymbolChild * this,
-	struct RlcSrcFile * file,
-	struct RlcParsedScopeEntry * parsed)
+	struct RlcSrcFile const * file,
+	struct RlcParsedSymbolChild const * parsed)
 {
 	RLC_DASSERT(this != NULL);
 	RLC_DASSERT(parsed != NULL);
 
-	switch(RLC_DERIVING_TYPE(parsed))
+	switch(parsed->fType)
 	{
-	case kRlcParsedConstructor:
-		this->type = kRlcScopedSymbolChildTypeConstructor;
-		this->name = NULL;
+	case kRlcParsedSymbolChildTypeConstructor:
+		*this = kRlcScopedSymbolChildConstructor;
 		break;
-	case kRlcParsedDestructor:
-		this->type = kRlcScopedSymbolChildTypeDestructor;
-		this->name = NULL;
+	case kRlcParsedSymbolChildTypeDestructor:
+		*this = kRlcScopedSymbolChildDestructor;
 		break;
-	default:
+	case kRlcParsedSymbolChildTypeIdentifier:
 		RLC_DASSERT(parsed->fName.length != 0);
 		this->type = kRlcScopedSymbolChildTypeIdentifier;
 		this->name = rlc_src_string_cstr(&parsed->fName, file);
+		break;
+	default:
+		RLC_DASSERT(!"unhandled symbol child type");
 	}
 }
 
@@ -51,4 +60,41 @@ int rlc_scoped_symbol_child_compare(
 		return strcmp(a->name, b->name);
 	} else
 		return 0;
+}
+
+void rlc_scoped_symbol_create(
+	struct RlcScopedSymbol * this,
+	struct RlcSrcFile const * file,
+	struct RlcParsedSymbol const * parsed)
+{
+	RLC_DASSERT(this != NULL);
+	RLC_DASSERT(file != NULL);
+	RLC_DASSERT(parsed != NULL);
+
+	this->fIsAbsolute = parsed->fIsRoot;
+	this->fChildren = NULL;
+	this->fChildCount = parsed->fChildCount;
+
+	rlc_malloc(
+		(void**)&this->fChildren,
+		sizeof(struct RlcScopedSymbolChild) * this->fChildCount);
+
+	for(RlcSrcIndex i = 0; i < this->fChildCount; i++)
+		rlc_scoped_symbol_child_create(
+			&this->fChildren[i],
+			file,
+			&parsed->fChildren[i]);
+}
+
+void rlc_scoped_symbol_destroy(
+	struct RlcScopedSymbol * this)
+{
+	RLC_DASSERT(this != NULL);
+	if(this->fChildCount)
+	{
+		for(RlcSrcIndex i = 0; i < this->fChildCount; i++)
+			rlc_scoped_symbol_child_destroy(&this->fChildren[i]);
+		rlc_free((void**)&this->fChildren);
+		this->fChildCount = 0;
+	}
 }
--- src/scoper/symbol.h
+++ src/scoper/symbol.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "../src/string.h"
-#include "../parser/scopeentry.h"
+#include "../parser/symbol.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -37,14 +37,52 @@ struct RlcScopedSymbol
 	RlcSrcSize fChildCount;
 };
 
-
+/** Creates a scoped symbol child from a parsed symbol child.
+@memberof RlcScopedSymbolChild
+@param[out] this:
+	The symbol child to create.
+	@dassert @nonnull
+@param[in] file:
+	The source file.
+	@dassert @nonnull
+@param[in] parsed:
+	The parsed symbol child. */
 void rlc_scoped_symbol_child_create(
 	struct RlcScopedSymbolChild * this,
-	struct RlcSrcFile * file,
-	struct RlcParsedScopeEntry * parsed);
+	struct RlcSrcFile const * file,
+	struct RlcParsedSymbolChild const * parsed);
 
+/** Destroys a scoped symbol child.
+@memberof RlcScopedSymbolChild
+@param[in,out] this:
+	The scoped symbol child to destroy.
+	@dassert @nonnull */
 void rlc_scoped_symbol_child_destroy(
 	struct RlcScopedSymbolChild * this);
+
+/** Creates a scoped symbol from a parsed symbol.
+@memberof RlcScopedSymbol
+@param[out] this:
+	The scoped symbol to create.
+	@dassert @nonnull
+@param[in] file:
+	The source file.
+	@dassert @nonnull
+@param[in] parsed:
+	The parsed symbol.
+	@dassert @nonnull */
+void rlc_scoped_symbol_create(
+	struct RlcScopedSymbol * this,
+	struct RlcSrcFile const * file,
+	struct RlcParsedSymbol const * parsed);
+
+/** Destroys a scoped symbol.
+@memberof RlcScopedSymbol
+@param[in,out] this:
+	The scope symbol to destroy.
+	@dassert @nonnull */
+void rlc_scoped_symbol_destroy(
+	struct RlcScopedSymbol * this);
 
 #ifdef __cplusplus
 }
