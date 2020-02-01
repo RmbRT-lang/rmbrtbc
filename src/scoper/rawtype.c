7e7e425a54c50c0c922bad58cb1869365c4820eb
RmbRT <steffen.rattay@gmail.com>
2020-02-01 21:46:30 +0100

    Added scoped rawtypes.

---
+++ src/scoper/rawtype.c
@@ -0,0 +1,58 @@
+#include "rawtype.h"
+#include "expression.h"
+#include "member.h"
+#include "scope.h"
+#include "../parser/rawtype.h"
+#include "../assert.h"
+#include "../malloc.h"
+
+#define kRlcScopedRawtype kRlcParsedRawtype
+
+struct RlcScopedRawtype * rlc_scoped_rawtype_new(
+	struct RlcSrcFile const * file,
+	struct RlcParsedRawtype const * parsed,
+	struct RlcScopedScope * parent)
+{
+	RLC_DASSERT(file != NULL);
+	RLC_DASSERT(parsed != NULL);
+	RLC_DASSERT(parent != NULL);
+
+	struct RlcScopedRawtype * ret = NULL;
+	rlc_malloc((void**)&ret, sizeof(struct RlcScopedRawtype));
+
+	rlc_scoped_scope_entry_create(
+		RLC_BASE_CAST(ret, RlcScopedScopeEntry),
+		file,
+		RLC_BASE_CAST(parsed, RlcParsedScopeEntry),
+		parent,
+		kRlcScopedRawtype);
+
+	ret->fSize = rlc_scoped_expression_new(parsed->fSize, file);
+	struct RlcScopedScopeItem * retAsItem = RLC_BASE_CAST(
+		RLC_BASE_CAST(ret, RlcScopedScopeEntry),
+		RlcScopedScopeItem);
+
+	for(RlcSrcIndex i = 0; i < parsed->fMembers.fEntryCount; i++)
+	{
+		rlc_scoped_scope_add_member(
+			retAsItem->children,
+			file,
+			parsed->fMembers.fEntries[i],
+			retAsItem);
+	}
+
+	return ret;
+}
+
+void rlc_scoped_rawtype_delete(
+	struct RlcScopedRawtype * this)
+{
+	RLC_DASSERT(this != NULL);
+
+	rlc_scoped_scope_entry_destroy_base(
+		RLC_BASE_CAST(this, RlcScopedScopeEntry));
+
+	rlc_scoped_expression_delete_virtual(this->fSize);
+
+	rlc_free((void**)&this);
+}
---
+++ src/scoper/rawtype.h
@@ -0,0 +1,57 @@
+/** @file rawtype.h
+	Contains the scoped rawtype type. */
+#ifndef __rlc_scoper_rawtype_h_defined
+#define __rlc_scoper_rawtype_h_defined
+#pragma once
+
+#include "scopeentry.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+struct RlcParsedRawtype;
+struct RlcScopedExpression;
+
+/** Rawtype class as used by the scoper.
+@extends RlcScopedScopeEntry */
+struct RlcScopedRawtype
+{
+	RLC_DERIVE(struct,RlcScopedScopeEntry);
+	/** The rawtype's size expression. */
+	struct RlcScopedExpression * fSize;
+};
+
+/** Creates a scoped rawtype from a parsed rawtype.
+@memberof RlcScopedRawtype
+@param[in] file:
+	The source file.
+	@dassert @nonnull
+@param[in] parsed:
+	The parsed rawtype.
+	@dassert @nonnull
+@param[in] parent:
+	The parent scope.
+	@dassert @nonnull
+@return
+	The scoped rawtype. */
+struct RlcScopedRawtype * rlc_scoped_rawtype_new(
+	struct RlcSrcFile const * file,
+	struct RlcParsedRawtype const * parsed,
+	struct RlcScopedScope * parent);
+
+/** Deletes a scoped rawtype.
+@memberof RlcScopedRawtype
+@param[in,out] this:
+	The rawtype to delete.
+	@dassert @nonnull */
+void rlc_scoped_rawtype_delete(
+	struct RlcScopedRawtype * this);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
