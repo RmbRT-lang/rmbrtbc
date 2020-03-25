d2a65238e04dd6d3873146342582a639a8eb6fdc
RmbRT <steffen.rattay@gmail.com>
2020-03-25 20:49:37 +0100

    Implemented scoped type name ctor/dtor.

--- src/scoper/typename.c
+++ src/scoper/typename.c
@@ -1,5 +1,10 @@
 #include "typename.h"
+#include "symbol.h"
+#include "functionsignature.h"
+#include "expression.h"
 #include "../macros.h"
+#include "../malloc.h"
+#include "../parser/typename.h"
 
 void rlc_scoped_type_name_create(
 	struct RlcScopedTypeName * this,
@@ -10,7 +15,43 @@ void rlc_scoped_type_name_create(
 	RLC_DASSERT(file != NULL);
 	RLC_DASSERT(parsed != NULL);
 
-	RLC_ASSERT(!"not implemented yet");
+	this->fName = NULL;
+	this->fParsed = parsed;
+	switch(parsed->fValue)
+	{
+	default:
+		RLC_DASSERT(!"unhandled enum value");
+	case kRlcParsedTypeNameValueName:
+		{
+			rlc_malloc(
+				(void**)&this->fName,
+				sizeof(struct RlcScopedSymbol));
+			rlc_scoped_symbol_create(
+				this->fName,
+				file,
+				parsed->fName);
+		} break;
+	case kRlcParsedTypeNameValueFunction:
+		{
+			rlc_malloc(
+				(void**)&this->fFunction,
+				sizeof(struct RlcScopedFunctionSignature));
+			rlc_scoped_function_signature_create(
+				this->fFunction,
+				file,
+				parsed->fFunction);
+		} break;
+	case kRlcParsedTypeNameValueExpression:
+		{
+			this->fExpression = rlc_scoped_expression_new(
+				parsed->fExpression,
+				file);
+		} break;
+	case kRlcParsedTypeNameValueVoid:
+		{
+			;
+		} break;
+	}
 }
 
 void rlc_scoped_type_name_destroy(
@@ -18,5 +59,30 @@ void rlc_scoped_type_name_destroy(
 {
 	RLC_DASSERT(this != NULL);
 
-	RLC_ASSERT(!"not implemented yet");
+	switch(this->fParsed->fValue)
+	{
+	default:
+		RLC_DASSERT(!"unhandled enum value");
+	case kRlcParsedTypeNameValueName:
+		{
+			rlc_scoped_symbol_destroy(this->fName);
+			rlc_free((void**)&this->fName);
+		} break;
+	case kRlcParsedTypeNameValueFunction:
+		{
+			rlc_scoped_function_signature_destroy(
+				this->fFunction,
+				this->fParsed->fFunction);
+			rlc_free((void**)&this->fFunction);
+		} break;
+	case kRlcParsedTypeNameValueExpression:
+		{
+			rlc_scoped_expression_delete_virtual(this->fExpression);
+			this->fExpression = NULL;
+		} break;
+	case kRlcParsedTypeNameValueVoid:
+		{
+			;
+		} break;
+	}
 }
