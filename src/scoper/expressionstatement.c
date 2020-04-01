8a550b5c77baebcd04f1fd297e3541eb96e77259
RmbRT <steffen.rattay@gmail.com>
2020-04-02 00:59:46 +0200

    Added scoped expression statements.

---
+++ src/scoper/expressionstatement.c
@@ -0,0 +1,36 @@
+#include "expressionstatement.h"
+#include "expression.h"
+#include "../parser/expressionstatement.h"
+#include "../assert.h"
+
+#define kRlcScopedExpressionStatement kRlcParsedExpressionStatement
+
+void rlc_scoped_expression_statement_create(
+	struct RlcScopedExpressionStatement * this,
+	struct RlcSrcFile const * file,
+	struct RlcParsedExpressionStatement const * parsed)
+{
+	RLC_DASSERT(this != NULL);
+	RLC_DASSERT(file != NULL);
+	RLC_DASSERT(parsed != NULL);
+
+	rlc_scoped_statement_create(
+		RLC_BASE_CAST(this, RlcScopedStatement),
+		RLC_BASE_CAST(parsed, RlcParsedStatement),
+		kRlcScopedExpressionStatement);
+
+	this->expression = rlc_scoped_expression_new(
+		parsed->fExpression,
+		file);
+}
+
+void rlc_scoped_expression_statement_destroy(
+	struct RlcScopedExpressionStatement * this)
+{
+	RLC_DASSERT(this != NULL);
+
+	rlc_scoped_statement_destroy_base(
+		RLC_BASE_CAST(this, RlcScopedStatement));
+
+	rlc_scoped_expression_delete_virtual(this->expression);
+}
---
+++ src/scoper/expressionstatement.h
@@ -0,0 +1,53 @@
+/** @file expressionstatement.h
+	Contains the scoped expression statement type. */
+#ifndef __rlc_scoper_expressionstatement_h_defined
+#define __rlc_scoper_expressionstatement_h_defined
+#pragma once
+
+#include "statement.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+struct RlcParsedExpressionStatement;
+
+/** Scoped expression statement type.
+@extends RlcScopedStatement */
+struct RlcScopedExpressionStatement
+{
+	RLC_DERIVE(struct,RlcScopedStatement);
+	/** The statement's expression. */
+	struct RlcScopedExpression * expression;
+};
+
+/** Creates a scoped expression statement from a parsed expression statement.
+@memberof RlcScopedExpressionStatement
+@param[out] this:
+	The scoped expression statement to create.
+	@dassert @nonnull
+@param[in] file:
+	The source file.
+	@dassert @nonnull
+@param[in] parsed:
+	The parsed expression statement.
+	@dassert */
+void rlc_scoped_expression_statement_create(
+	struct RlcScopedExpressionStatement * this,
+	struct RlcSrcFile const * file,
+	struct RlcParsedExpressionStatement const * parsed);
+
+/** Destroys a scoped expression statement.
+@memberof RlcScopedExpressionStatement
+@param[in,out] this:
+	The scoped expression statement to destroy.
+	@dassert @nonnull */
+void rlc_scoped_expression_statement_destroy(
+	struct RlcScopedExpressionStatement * this);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
