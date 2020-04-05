110be03b40fcb89ce7818886797b23783c524a84
RmbRT <steffen.rattay@gmail.com>
2020-04-05 17:44:52 +0200

    Added scoped break statements.

---
+++ src/scoper/breakstatement.c
@@ -0,0 +1,34 @@
+#include "breakstatement.h"
+#include "../parser/breakstatement.h"
+#include "../macros.h"
+
+#define kRlcScopedBreakStatement kRlcParsedBreakStatement
+
+void rlc_scoped_break_statement_create(
+	struct RlcScopedBreakStatement * this,
+	struct RlcSrcFile const * file,
+	struct RlcParsedBreakStatement const * parsed)
+{
+	RLC_DASSERT(this != NULL);
+	RLC_DASSERT(file != NULL);
+	RLC_DASSERT(parsed != NULL);
+
+	rlc_scoped_statement_create(
+		RLC_BASE_CAST(this, RlcScopedStatement),
+		RLC_BASE_CAST(parsed, RlcParsedStatement),
+		kRlcScopedBreakStatement,
+		0);
+
+	rlc_scoped_control_label_create(&this->label, file, &parsed->fLabel);
+}
+
+void rlc_scoped_break_statement_destroy(
+	struct RlcScopedBreakStatement * this)
+{
+	RLC_DASSERT(this != NULL);
+
+	rlc_scoped_control_label_destroy(&this->label);
+
+	rlc_scoped_statement_destroy_base(
+		RLC_BASE_CAST(this, RlcScopedStatement));
+}
