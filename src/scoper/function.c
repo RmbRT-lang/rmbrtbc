5ea3b89cbb675e322e164ba06cc2c52aa4929ccc
RmbRT <steffen.rattay@gmail.com>
2020-04-16 19:19:36 +0200

    Added scoped member function.

--- src/scoper/function.c
+++ src/scoper/function.c
@@ -1,6 +1,7 @@
 #include "expression.h"
 #include "function.h"
 #include "scope.h"
+#include "scopeitemgroup.h"
 #include "statement.h"
 #include "variable.h"
 #include "../parser/function.h"
@@ -8,34 +9,18 @@
 #include "../malloc.h"
 
 #define kRlcScopedFunction kRlcParsedFunction
+#define kRlcScopedMemberFunction kRlcParsedMemberFunction
 
-static inline struct RlcParsedFunction const * parsed(
-	struct RlcScopedFunction const * this)
-{
-	return RLC_DERIVE_CAST(
-		RLC_BASE_CAST(this, RlcScopedScopeEntry)->parsed,
-		RlcParsedScopeEntry,
-		struct RlcParsedFunction const);
-}
-
-void rlc_scoped_function_create(
+static void rlc_scoped_function_create(
 	struct RlcScopedFunction * this,
 	struct RlcSrcFile const * file,
 	struct RlcParsedFunction const * parsed,
-	struct RlcScopedScopeItemGroup * group)
+	struct RlcScopedScope * scope)
 {
 	RLC_DASSERT(this != NULL);
 	RLC_DASSERT(file != NULL);
 	RLC_DASSERT(parsed != NULL);
-	RLC_DASSERT(group != NULL);
-
-	rlc_scoped_scope_entry_create(
-		RLC_BASE_CAST(this, RlcScopedScopeEntry),
-		file,
-		RLC_BASE_CAST(parsed, RlcParsedScopeEntry),
-		group,
-		kRlcScopedFunction,
-		&parsed->fTemplates);
+	RLC_DASSERT(scope != NULL);
 
 	this->arguments = NULL;
 	if(parsed->fArgumentCount)
@@ -46,14 +31,14 @@ void rlc_scoped_function_create(
 	for(RlcSrcIndex i = 0; i < parsed->fArgumentCount; i++)
 	{
 		struct RlcScopedScopeEntry * it = rlc_scoped_scope_add_entry(
-			RLC_BASE_CAST2(this, RlcScopedScopeEntry, RlcScopedScopeItem)->children,
+			scope,
 			file,
 			RLC_BASE_CAST(&parsed->fArguments[i], RlcParsedScopeEntry));
 
 		this->arguments[i] = RLC_DERIVE_CAST(
 			it,
 			RlcScopedScopeEntry,
-			struct RlcScopedVariable);
+			struct RlcScopedGlobalVariable);
 	}
 
 	if(parsed->fHasReturnType)
@@ -75,27 +60,102 @@ void rlc_scoped_function_create(
 	}
 }
 
-void rlc_scoped_function_destroy(
+static void rlc_scoped_function_destroy(
 	struct RlcScopedFunction * this)
 {
 	RLC_DASSERT(this != NULL);
 
-	struct RlcParsedFunction const * p = parsed(this);
-
-	if(p->fArgumentCount)
+	if(this->argumentCount)
 		rlc_free((void**)&this->arguments);
 
-	if(p->fHasReturnType)
+	if(this->hasReturnType)
 		rlc_scoped_type_name_destroy(&this->returnType);
 
-	if(p->fHasBody)
+	if(this->hasBody)
 	{
-		if(p->fIsShortHandBody)
+		if(this->isShortHandBody)
 			rlc_scoped_expression_delete_virtual(this->body.expression);
 		else
 			rlc_scoped_statement_delete(this->body.statement);
 	}
+}
+
+
+void rlc_scoped_global_function_create(
+	struct RlcScopedGlobalFunction * this,
+	struct RlcSrcFile const * file,
+	struct RlcParsedFunction const * parsed,
+	struct RlcScopedScopeItemGroup * group)
+{
+	RLC_DASSERT(this != NULL);
+	RLC_DASSERT(file != NULL);
+	RLC_DASSERT(parsed != NULL);
+	RLC_DASSERT(group != NULL);
+
+	rlc_scoped_scope_entry_create(
+		RLC_BASE_CAST(this, RlcScopedScopeEntry),
+		file,
+		RLC_BASE_CAST(parsed, RlcParsedScopeEntry),
+		group,
+		kRlcScopedFunction,
+		&parsed->fTemplates);
+
+	rlc_scoped_function_create(
+		RLC_BASE_CAST(this, RlcScopedFunction),
+		file,
+		parsed,
+		RLC_BASE_CAST2(this, RlcScopedScopeEntry, RlcScopedScopeItem)->children);
+}
+
+void rlc_scoped_global_function_destroy(
+	struct RlcScopedGlobalFunction * this)
+{
+	RLC_DASSERT(this != NULL);
+
+	rlc_scoped_function_destroy(
+		RLC_BASE_CAST(this, RlcScopedFunction));
 
 	rlc_scoped_scope_entry_destroy_base(
 		RLC_BASE_CAST(this, RlcScopedScopeEntry));
+}
+
+void rlc_scoped_member_function_create(
+	struct RlcScopedMemberFunction * this,
+	struct RlcSrcFile const * file,
+	struct RlcParsedMemberFunction const * parsed,
+	struct RlcScopedScopeItemGroup * scope)
+{
+	RLC_DASSERT(this != NULL);
+	RLC_DASSERT(file != NULL);
+	RLC_DASSERT(parsed != NULL);
+	RLC_DASSERT(scope != NULL);
+
+	rlc_scoped_member_create(
+		RLC_BASE_CAST(this, RlcScopedMember),
+		file,
+		RLC_BASE_CAST(parsed, RlcParsedMember),
+		scope->parent,
+		kRlcScopedMemberFunction,
+		&RLC_BASE_CAST(parsed, RlcParsedFunction)->fTemplates);
+
+	rlc_scoped_function_create(
+		RLC_BASE_CAST(this, RlcScopedFunction),
+		file,
+		RLC_BASE_CAST(parsed, RlcParsedFunction),
+		RLC_BASE_CAST2(
+			this,
+			RlcScopedMember,
+			RlcScopedScopeItem)->children);
+}
+
+void rlc_scoped_member_function_destroy(
+	struct RlcScopedMemberFunction * this)
+{
+	RLC_DASSERT(this != NULL);
+
+	rlc_scoped_member_destroy_base(
+		RLC_BASE_CAST(this, RlcScopedMember));
+
+	rlc_scoped_function_destroy(
+		RLC_BASE_CAST(this, RlcScopedFunction));
 }
