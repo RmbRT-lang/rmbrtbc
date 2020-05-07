1643d78690f8a5dbf1b69fbe2606ff53bedbf8a0
RmbRT <steffen.rattay@gmail.com>
2020-05-07 22:52:28 +0200

    Renamed enum.name() -> enum.NAME().
    This allows enum constants named 'name'.

--- src/parser/enum.c
+++ src/parser/enum.c
@@ -255,7 +255,7 @@ static void rlc_parsed_enum_print_to_file(
 		file,
 		out);
 	fprintf(out, ", %zu>::EnumWrapper;\n\n"
-		"constexpr ::__rl_char const * name() const\n"
+		"constexpr ::__rl_char const * NAME() const\n"
 		"{\n\t"
 		"::__rl_char const * const names[] = {\n",
 		(size_t)this->fConstantCount);
